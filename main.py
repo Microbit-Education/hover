54099b8798ef733079d9b603b3f5a848f7f2cc58
maya-e-s <76231198+maya-e-s@users.noreply.github.com>
2022-06-07 17:50:40 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+def on_button_pressed_a():
+    basic.pause(2000)
+    Drones.Basic_action(Drones.Basicoptions.TAKEOFF)
+    Drones.hovering(10)
+    Drones.Basic_action(Drones.Basicoptions.LANDING)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+Drones.init_module(Drones.Runmodes.MASTER)
