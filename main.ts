7c5c802952ebb4489d9afc401a3f0a4f5bf35ac1
Maya Stevenson <maya.stevenson@gmail.com>
2022-06-26 17:46:02 -0400

    adding icon from VS code

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onButtonPressed(Button.A, function () {
+    basic.show_icon(IconNames.HEART)
     basic.pause(2000)
     Drones.Basic_action(Drones.Basicoptions.Takeoff)
     Drones.Hovering(10)
